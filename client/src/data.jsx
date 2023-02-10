ad600502025f3ba071d7c14512efeb80d9937ad4
James R Hoggard III <jamesrhoggardiii@Jamess-MacBook-Air-2.local>
2023-02-10 17:01:55 -0500

    prod01

---
+++ client/src/data.jsx
@@ -0,0 +1,38 @@
+let initialProducts = [
+  {
+    title: 'Brother-MFC20x',
+    url: 'https://rjs.org/',
+    author: 'Joan Vicke',
+    num_comms: 3,
+    points: 4887,
+    objectID: 0,
+  },
+  {
+    title: 'Brother-MFC10x',
+    url: 'https://www.js.org/',
+    author: 'Ervin Kimb',
+    num_comms: 2,
+    points: 5007,
+    objectID: 1,
+  },
+  {
+    title: 'Epson-C5560i',
+    url: 'https://www.js.org/',
+    author: 'Mary Kib',
+    num_comms: 2,
+    points: 5007,
+    objectID: 2,
+  },
+  {
+    title: 'Canon-PP7C0rx',
+    url: 'https://www.js.org/',
+    author: 'B. Montgomery',
+    num_comms: 2,
+    points: 5007,
+    objectID: 3,
+  },
+];
+
+export function getInitProducts(){
+  return initialProducts;
+}
